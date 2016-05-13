c58b5db2b2f6229f6f67cea25d0d17dcb6cb2a00
Sumit Sati <sumit.sati@crisp-da.de>
2016-05-13 16:47:33 +0200

    task 1 completed. comments pending

--- task1/1.c
+++ task1/1.c
@@ -12,6 +12,8 @@
 #include<string.h>
 //Header file for wait calls
 #include<sys/wait.h>
+//Header file for random seed 
+#include<time.h>
 #define FORK_WAIT 250000
 #define CHILD_WAIT 500000
 
@@ -29,6 +31,18 @@ int main(int argc, char* argv[]){
 		}
 		printf("Child process: my pid is %d, parent pid is %d\n",
                getpid(), getppid());
+		if(argc>1){
+//			printf("This thing will work for random failure\n");
+			int random_number;
+			srand(time(NULL)); //initialize random generator
+			random_number = rand();
+			if (random_number % 2 ==0){
+				return EXIT_SUCCESS;
+			}
+			else{
+				return EXIT_FAILURE;
+			}
+		}
 		return EXIT_SUCCESS;
 	}
 	else if (p_return_val > 0){ // parent process
@@ -43,8 +57,17 @@ int main(int argc, char* argv[]){
 			printf("Child:%d, we were waiting for, is terminated\n"
 					,p_return_val);
 			if(WIFEXITED(child_status)){
-				printf("Child terminated normally with exit status"
+				if(WEXITSTATUS(child_status)==0){
+					printf("Child terminated normally with exit status"
 					": %d\n",WEXITSTATUS(child_status));
+					return EXIT_SUCCESS;
+				}
+				else{
+					printf("Child terminated abnormally with exit status"
+					": %d\n",WEXITSTATUS(child_status));
+					return EXIT_FAILURE;
+				}
+				
 			}
 			else{
 				printf("Child exited abnormally. Exit status:"
@@ -65,7 +88,7 @@ int main(int argc, char* argv[]){
 		return EXIT_FAILURE;
 	}
 	else{
-		printf("This should never happen since all cases are"
+		printf("This should never reach since all cases are"
 				" covered by other if else branches");
 		return EXIT_FAILURE;
 	}
--- task1/test_script.sh
+++ task1/test_script.sh
@@ -2,10 +2,28 @@
 EXE_F=prog
 count=5
 counter=1
+succ=0
+fail=0
+LOG_FILE=run.log
 #set -x
+if [ -f $LOG_FILE ]
+then 
+	rm $LOG_FILE
+fi
+
+#2>&1
+#1>$LOG_FILE
+exec &>$LOG_FILE
+
 while [ "$counter" -le $count ] 
 do
-	./$EXE_F |tee -a run.log
+	./$EXE_F 2 #|tee -a $LOG_FILE
+	if [ $? -eq 0 ]; then 
+		 succ=`expr $succ + 1`
+	else 
+		fail=`expr $fail + 1`
+	fi
 	counter=`expr $counter + 1 `
 done
+echo "Succeeded $succ times and Failed $fail times"
 #set +x
