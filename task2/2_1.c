9074efe5d185c973fe8edeab82c4c4b320bec582
Sumit Sati <sumit.sati@stud.tu-darmstadt.de>
2016-05-15 08:48:28 +0200

    minor modifications

--- task2/2_1.c
+++ task2/2_1.c
@@ -1,25 +1,20 @@
 #define _BSD_SOURCE // used for usleep function
-//Standard input output header file
-#include<stdio.h>
-//Header file for process creation, identifications and types.
-#include<sys/types.h>
-#include<unistd.h>
-//Header file for process terminations
-#include<stdlib.h>
-//Header file for error number
-#include<errno.h>
-//Header file for string processing
-#include<string.h>
-//Header file for wait calls
-#include<sys/wait.h>
-//Header file for random seed 
-#include<time.h>
+#include<stdio.h> //Standard input output header file
+#include<sys/types.h> //Header file for process creation, identifications and types.
+#include<unistd.h> //Header file for getopt processing
+#include<stdlib.h> //Header file for process terminations
+#include<errno.h> //Header file for error number
+#include<string.h> //Header file for string processing
+#include<sys/wait.h> //Header file for wait calls
+#include<time.h> //Header file for random seed 
+#define MAX_FORKS 10
 int backup();
 void wait_for_request(int);
 void process_request();
 int counter=0;
 int main(int argc, char* argv[]){
 	printf("Parent process started with pid: %d \n",getpid());
+//TODO	while ()
 	int ft = backup();
 // Do parent stuff. backup process has started already.
 	while (1){
@@ -30,7 +25,7 @@ int main(int argc, char* argv[]){
 }
 int backup(){
 	int ret, restarts = 0;
-    for(;;restarts++){
+	for(;;restarts++){
         ret = fork();
         if (ret == 0){ // child process
             printf("Child process: my pid is %d, parent pid is %d\n",
@@ -40,12 +35,15 @@ int backup(){
 		else if (ret > 0 ){ //parent process
 			fprintf(stdout, "parent process continues\n");
 			while(ret != waitpid(ret,0,0)){
-				fprintf(stderr, "child dies, respawn");
+				fprintf(stderr, "child dies, respawn\n");
 			}
 		}
 		else {
-			fprintf(stderr,"Fork failed, %s\n",strerror(errno));
-			return EXIT_FAILURE;
+			if (restarts > MAX_FORKS){
+				fprintf(stderr,"Fork failed and num_forks exceeded: %d error: , %s\n",restarts,strerror(errno));
+				return restarts;
+			}
+			fprintf(stderr,"Fork failed retrying, %s\n",strerror(errno));
 		}
 	}
 }
@@ -54,7 +52,7 @@ void wait_for_request(int ft){
 		printf("usleep failed, error:%s\n",strerror(errno)); 
 		exit(-1); 
 	}
-	fprintf(stdout,"request %d found, process countis: %d\n",counter,ft);
+	fprintf(stdout,"request %d found, process count is: %d\n",counter,ft);
 }
 void process_request(){
 	if(usleep(1000000)!=0){ 
